edeb656707d042b30e281d263ae60c12e66e2412
a6hi <saraffabhishek@outlook.com>
2022-05-27 21:47:06 -0400

    Setting up index and script files

---
+++ script.js
@@ -0,0 +1,8 @@
+//This function randomly returns Rock/Paper/Scissors
+function computerPlay() {
+}
+
+//This function displays the result of the game
+function result(playerSelection, computerSelection) {
+
+}
