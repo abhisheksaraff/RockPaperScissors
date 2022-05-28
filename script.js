aeb7d52fdb7f1d34e40f8b8ebec147ec712e6226
a6hi <saraffabhishek@outlook.com>
2022-05-27 22:03:35 -0400

    Setting up Computer's Turn to get a random selection

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 //This function randomly returns Rock/Paper/Scissors
 function computerPlay() {
+    let play = Math.floor(Math.random() * 3 + 1);
+    //console.log(play);
+    switch(play)    {
+        case 1:
+            return("Rock");
+            break;
+        case 2:
+            return("Paper");
+            break;
+        case 3:
+            return("Scissor");
+            break;
+    }
 }
 
 //This function displays the result of the game
