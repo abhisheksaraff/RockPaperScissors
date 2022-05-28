4a89e33a8d2523b1acaf0c01a387291cb0abde28
a6hi <saraffabhishek@outlook.com>
2022-05-28 00:38:40 -0400

    Working game with pre-set user input

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
+let playerSelection = "RocK", computerSelection = computerPlay();
+console.log(result(playerSelection, computerSelection));
+
 //This function randomly returns Rock/Paper/Scissors
 function computerPlay() {
     let play = Math.floor(Math.random() * 3 + 1);
-    //console.log(play);
+    
     switch(play)    {
         case 1:
-            return("Rock");
+            return("ROCK");
             break;
         case 2:
-            return("Paper");
+            return("PAPER");
             break;
         case 3:
-            return("Scissor");
+            return("SCISSORS");
             break;
     }
 }
 
 //This function displays the result of the game
 function result(playerSelection, computerSelection) {
+    let result = selectionToInt(playerSelection) - selectionToInt(computerSelection);
+    
+    if(result == 0)    {
+        //Draw
+        return("It's a Draw!");
+    }   else if(result == -1 || result == 2) {
+        //Computer Wins
+        return("You Lose! " + computerSelection + " beats "+ playerSelection.toUpperCase());
+    }else {
+        //Player Wins
+        return("You Win! " + playerSelection.toUpperCase() + " beats "+ computerSelection);
+    }
+}
 
+//This function converts Player's input into a numerical value
+function selectionToInt(selection) {
+    if(selection.toUpperCase() == "ROCK"){
+        return 1;
+    } else if(selection.toUpperCase() == "PAPER") {
+        return 2;
+    } else if(selection.toUpperCase() == "SCISSORS") {
+        return 3;
+    }
 }
