f8d56bd75b6caf86de12c0077a2c9e000eb546bd
a6hi <saraffabhishek@outlook.com>
2022-08-02 00:40:09 -0400

    Display the running score, and announce a winner of the game once one player reaches 5 points

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 //node list of buttons
 const buttons = document.querySelectorAll("button");
 const resultDiv = document.querySelector("#result");
-let numOfGames = 0, result ="";
+let numOfGames = 0, result = "", playerWins = 0, computerWins = 0;
 
 //iterate through each item of buttons
 buttons.forEach((button) => {
 
     //add event listener to each item of buttons
     button.addEventListener('click', () => {
-        console.log(button.id);
-        //console.log(selectionToInt(computerPlay));
-        numOfGames++;
-        
-        //***WORKS HERE***
-        let test = Math.floor(Math.random() * 3 + 1);
-        console.log(test);
-        
-        let result = playRound(button.id, computerPlay);
-        console.log(result);
+        if (numOfGames != 5) {
+            numOfGames++;
+            let thisRound = playRound(button.id, computerPlay());
+            resultDiv.textContent = thisRound;
+
+            if (numOfGames == 5) {
+                endGame();
+            }
+        }
     });
 });
 
+//This function determines the result of the game and displays it to the console
+function playRound(playerSelection, computerSelection) {
+    let turn = (selectionToInt(playerSelection) - selectionToInt(computerSelection));
+
+    if (turn == 0) {
+        //Draw
+        return "It's a Draw, you both chose " + playerSelection;
+    } else if (turn == -1 || turn == 2) {
+        //Computer Wins
+        computerWins++;
+        return "You Lose! " + computerSelection + " beats " + playerSelection.toUpperCase();
+    } else {
+        //Player Wins
+        playerWins++
+        return "You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection;
+    }
+}
+
 //This function randomly returns Rock/Paper/Scissors
 function computerPlay() {
-
-    /*** DOESN'T WORK HERE ***/
     let play = Math.floor(Math.random() * 3 + 1);
 
     switch (play) {
         case 1:
-            return ("rock");
+            return ("ROCK");
             break;
         case 2:
-            return ("paper");
+            return ("PAPER");
             break;
         case 3:
-            return ("scissors");
+            return ("SCISSORS");
             break;
     }
-}
-
-//This function determines the result of the game and displays it to the console
-function playRound(playerSelection, computerSelection) {
-    return (selectionToInt(playerSelection) - selectionToInt(computerSelection));
-
 }
 
 //This function converts Player's input into a numerical value
@@ -57,37 +66,15 @@ function selectionToInt(selection) {
     }
 }
 
-/*
-//Plays 5 rounds of the game
-function game() {
-    let playerWins = 0, computerWins = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter your choice: ROCK / PAPER / SCISSORS:");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result == 0) {
-            //Draw
-            console.log("It's a Draw!");
-        } else if (result == -1 || result == 2) {
-            //Computer Wins
-            computerWins++;
-            console.log("You Lose! " + computerSelection + " beats " + playerSelection.toUpperCase());
-        } else {
-            //Player Wins
-            playerWins++
-            console.log("You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection);
-        }
-    }
-
-    //Display the result for 5 rounds
+//Calculates and Displays the Winner
+function endGame()    {
     if (playerWins == computerWins)
-        console.log("It was a Draw!");
+        result = "It was a Draw!";
     else if (playerWins > computerWins) {
-        console.log("You Win!");
+        result = "You Win " + playerWins + " to " + computerWins + "!";
     } else {
-        console.log("You Lost!");
+        result = "You Lost " + playerWins + " to " + computerWins + "!";
     }
+
+    resultDiv.textContent = result;
 }
-*/
